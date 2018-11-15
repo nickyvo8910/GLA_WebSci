ae30674ecfd69b80f5596dc0626438ef5e9a000f
Nicky Vo <nickyvo8910@gmail.com>
2018-11-15 18:28:50 +0000

    DONE BASIC CRAWLER

--- basic.py
+++ basic.py
@@ -11,16 +11,28 @@ consumer_key = "VIGGAj16rqqaEOEEJjVWaROpi"
 consumer_secret = "HAqUGIRXMvRDkDTdH2fJ1pxsjoRARbepz5rsSeHJWYl2eve1Bl"
 access_token = "969038491706363905-VFcCIGYkGRAeG6CPh1kGCPbj8fiHXwo"
 access_token_secret = "SkEve79Dkoha1r7wVn34VLyHLendlenfAmemKOpBjxdow"
-db =''
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+mongo = MongoClient('localhost', 27017)
+db = mongo['tweet']['basic']
+
 
 
 class MyStreamListener(tweepy.StreamListener):
+
+    def on_connect(self):
+        print("Connected")
 
     def on_status(self, status):
         print(status.text).encode("utf-8")
 
     def on_data(self, data):
         try:
+            print("Do data")
             tweet = json.loads(data)
             db.insert_one(tweet)
             print("inserted")
@@ -32,24 +44,8 @@ class MyStreamListener(tweepy.StreamListener):
             print(traceback.format_exc())
 
 def _main():
-    print("Done Importing")
+    print("Done Importing & Config")
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    api = tweepy.API(auth)
-
-    # public_tweets = api.home_timeline()
-    #
-    # for tweet in public_tweets:
-    #     print(tweet.)
-    #     print(tweet.text.encode("utf-8"))
-
-    mongo = MongoClient('localhost', 27017)
-    db = mongo['tweet']['basic']
-
-
-    print("Connected DB without error")
 
     myStreamListener = MyStreamListener()
     myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
